ea2a5444603da0128c6f84da6bd53ecee9a38eb8
Jack Goh <1163977-jackgoh@users.noreply.gitlab.com>
2023-12-18 16:03:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+### Install nodejs
+`https://nodejs.org/en`
+
+### Open up terminal app
+
+### Clone this repo
+`git clone https://github.com/botahead/celestia-bot`
+
+### Install packages
+`cd celestia-bot && npm install`
+
+### Setup environtment file
+`cp .env.example .env`
+
+### Edit environtment file with your own seed and wallet address and save
+```
+SEED_PHRASE=your_seed_phrase_here
+WALLET_ADDRESS=your_wallet_address_here
+```
+
+### Run the script
+`node run.js`
---
+++ run.js
@@ -0,0 +1,46 @@
+const { DirectSecp256k1HdWallet } = require("@cosmjs/proto-signing");
+const { SigningStargateClient, GasPrice, calculateFee } = require("@cosmjs/stargate");
+require('dotenv').config();
+
+async function sendCosmosTokens() {
+    const mnemonic = process.env.SEED_PHRASE;
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'celestia' });
+    const [firstAccount] = await wallet.getAccounts();
+
+    const rpcEndpoint = "http://rpc.lunaroasis.net";
+    const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, wallet);
+
+    const transactionAmount = 1
+    const defaultGasPrice = GasPrice.fromString('160utia')
+    const defaultSendFee = calculateFee(80_000, defaultGasPrice)
+    const recipient = process.env.WALLET_ADDRESS;
+
+    // console.log('sender', firstAccount.address)
+    // console.log('transactionFee', defaultSendFee)
+    // console.log('amount', transactionAmount)
+
+    // Send a transaction
+    const transaction = await client.sendTokens(
+        firstAccount.address,
+        recipient,
+        [{ denom: "utia", amount: "1" }],
+        {
+            amount: [{ denom: "utia", amount: "1915" }],
+            gas: "100000",
+        },
+        "ZGF0YToseyJvcCI6Im1pbnQiLCJhbXQiOjEwMDAwLCJ0aWNrIjoiY2lhcyIsInAiOiJjaWEtMjAifQ=="
+    );
+
+    console.log('Successfully broadcasted:', transaction.transactionHash)
+}
+
+sendCosmosTokens().catch(err => {
+    console.error(err);
+});
+
+setInterval(() => {
+    console.log('Mint executing')
+    sendCosmosTokens().catch(err => {
+        console.error(err);
+    });
+}, 10000); // This sets the interval to 1 second
